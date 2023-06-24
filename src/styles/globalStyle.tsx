e693528aa5658ed5b1646038b38334931a257ec0
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-06-23 22:51:14 -0300

    adicionei o GlobalStyle

---
+++ src/styles/globalStyle.tsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #333333;
+    font-family: 'Rubik', sans-serif;
+  }
+`
+
+export default GlobalStyle;
