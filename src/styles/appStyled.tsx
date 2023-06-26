579a0ac16264c0468f1a27ddd96f032b9e4fb01a
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-06-25 22:48:45 -0300

    adicionei um gap nos cards

--- src/styles/appStyled.tsx
+++ src/styles/appStyled.tsx
@@ -3,6 +3,7 @@ import { styled } from "styled-components";
 const AppContainer = styled.div`
   align-items: center;
   display: flex;
+  gap: 2.5vw;
   height: 100vh;
   justify-content: center;
   width: 100%;
