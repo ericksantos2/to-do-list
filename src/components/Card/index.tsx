69b50e6eb28539f46c12033a279cf60a40ba344c
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-06-25 22:45:50 -0300

    adicionei os itens da to do list ao componente de card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,19 +1,36 @@
-import { CardDiv, ListaTodo } from "./styled";
+import { useDispatch, useSelector } from 'react-redux';
+import { CardDiv, ListaTodo } from './styled';
+import { BsFillTrashFill } from 'react-icons/bs';
+import { RootState } from '../../store';
+import { deleteTodoItem, toggleCheck } from '../../store/reducers/todoList';
+import { ActionCreatorWithPayload } from '@reduxjs/toolkit';
 
 export default function Card() {
+  const dispatch = useDispatch();
+  const todoList = useSelector((store: RootState) => store.todoListSlice);
+
+  const handleDispatch = (action: ActionCreatorWithPayload<number, string>, index: number) => () => {
+    dispatch(action(index));
+  }
+
   return (
     <CardDiv>
-      <h1>Coisas para fazer</h1>
+      <h1>Tarefas</h1>
       <ListaTodo>
-        <li>
-          <input type='checkbox' />
-          <label htmlFor="">zap 2</label>
-        </li>
-        <li>
-          <input type='checkbox' id='zap' />
-          <label htmlFor="zap">zap 3</label>
-        </li>
+        {todoList.length !== 0 ? (
+          todoList.map((item, index) => (
+            <li key={item.id} style={{ textDecoration: item.checked ? 'line-through' : 'none'}}>
+              <input type='checkbox' id={item.id} checked={item.checked} onChange={handleDispatch(toggleCheck, index)} />
+              <label htmlFor={item.id} style={{opacity: item.checked ? '0.7' : '1' }} >{item.text}</label>
+              {item.checked && (
+                <button onClick={handleDispatch(deleteTodoItem, index)}><BsFillTrashFill/></button>
+              )}
+            </li>
+          ))
+        ) : (
+          <p style={{opacity: 0.6}} >nenhuma por enquanto</p>
+        )}
       </ListaTodo>
     </CardDiv>
-  )
+  );
 }
