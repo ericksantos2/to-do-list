0d76e517febe4d145cccf63f567cbf749c80f8cf
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-06-25 22:42:34 -0300

    adicionei o provider do redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import 'reset.css'
-import GlobalStyle from './styles/globalStyle.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import 'reset.css';
+import GlobalStyle from './styles/globalStyle.tsx';
+import { Provider } from 'react-redux';
+import store from './store/index.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
