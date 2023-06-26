9f7b956f6b6e34b0a80e9a9c99934314ecce2add
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-06-25 22:49:00 -0300

    mudei a fonte por uma variável

--- src/styles/globalStyle.tsx
+++ src/styles/globalStyle.tsx
@@ -3,7 +3,10 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: #333333;
-    font-family: 'Rubik', sans-serif;
+    font-family: var(--font-family);
+  }
+  :root {
+    --font-family: 'Rubik', sans-serif;
   }
 `
 
